051862a15bcb45b7d2993df698c76f391bb7b3a6
Jonathan S <jonastrijker@gmail.com>
2022-11-01 21:04:38 +0100

    environment var test?

--- export.sh
+++ export.sh
@@ -22,5 +22,6 @@ export SPARK_HOME=/var/scratch/$USER/spark;
 export PATH=${SPARK_HOME}/bin:${PATH};
 export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
 export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.8-src.zip:$PYTHONPATH
+export PYSPARK_PYTHON=SPARK_HOME/bin/pyspark
 EOF
 source ~/.bashrc
