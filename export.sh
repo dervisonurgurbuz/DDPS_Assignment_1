37e1c0112cc0ebde322ce6edc0db824dc9775045
Jonathan S <jonastrijker@gmail.com>
2022-11-01 21:17:56 +0100

    changes to export.sh

--- export.sh
+++ export.sh
@@ -20,8 +20,11 @@ alias java="$JAVA_HOME/bin/java"
 #SPARK
 export SPARK_HOME=/var/scratch/$USER/spark;
 export PATH=${SPARK_HOME}/bin:${PATH};
-export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
-export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.8-src.zip:$PYTHONPATH
-export PYSPARK_PYTHON=$SPARK_HOME/bin/pyspark
+# export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH;
+# export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.8-src.zip:$PYTHONPATH;
+# export PYSPARK_PYTHON=$SPARK_HOME/bin/pyspark;
+
+# EASE OF USE
+export GIT_HOME=/var/scratch/$USER/DDPS_Assignment_1;
 EOF
 source ~/.bashrc
