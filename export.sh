c06d3930840d476c644d48b4836aac2749a91d77
Jonathan S <jonastrijker@gmail.com>
2022-11-01 20:18:19 +0100

    pythonpath?

--- export.sh
+++ export.sh
@@ -19,5 +19,6 @@ alias java="$JAVA_HOME/bin/java"
 #SPARK
 export SPARK_HOME=/var/scratch/$USER/spark;
 export PATH=${SPARK_HOME}/bin:${PATH};
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
 EOF
 source ~/.bashrc
