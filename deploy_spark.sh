5b382abc0b271a89e1c171065cfa5f4d935c0785
dervisonurgurbuz <onurgurbuz97@gmail.com>
2022-11-02 20:12:35 +0100

    Implemented experiment 2 Interactive Data Mining

--- deploy_spark.sh
+++ deploy_spark.sh
@@ -34,4 +34,4 @@ echo "export JAVA_HOME=/var/scratch/$USER/jdk-11.0.2" >> spark-env.sh
 echo "export SPARK_MASTER_HOST=$master" >> spark-env.sh
 echo "$worker" > workers
 
-ssh $master "cd /var/scratch/$USER/spark && ./bin/spark-submit examples/src/main/python/pi.py 1000" 
+ssh $master "cd /var/scratch/$USER/spark && ./bin/spark-submit examples/src/main/python/main.py 1000"
