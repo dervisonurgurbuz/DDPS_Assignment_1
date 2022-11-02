126f22bcabc744d94679991a9a0c4f9f04dd8cd5
dervisonurgurbuz <onurgurbuz97@gmail.com>
2022-11-02 22:42:29 +0100

    Other Experiments have been implemented

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import title
 
 import value as value
 from pyspark.shell import sc
+
 from pyspark.sql.functions import when, split
 
 
@@ -19,66 +20,32 @@ if __name__ == '__main__':
     print_hi('This is just a beginning :)')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# Pyspark Introduction
-
-# import pyspark
-# from pyspark.sql import SparkSession
-# from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-# from pyspark.sql.types import ArrayType, DoubleType, BooleanType
-# from pyspark.sql.functions import col, array_contains
-#
-# spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-#
-# df = spark.read.option("header", True).csv("./zipcodes.csv")
-#
-# df.printSchema()
-#
+import time
 import pyspark
 from pyspark.sql import SparkSession
 
-# spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-
-#
-# fileRDD = sc.wholeTextFiles("./Files")
-#
 # rdd = fileRDD.collect()
 #
 # columns = ["Platform","Page","View Rank"]
 # df = spark.createDataFrame(data=rdd, schema = columns)
 # df.show()
-# # rdd.show()
-# # print Data
+# rdd.show()
+# print Data
 # for row in rdd: {
 #
 #     print(row)
 # }
 
-
-# spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-# data = [("James","Smith","USA","CA"),
-#     ("Michael","Rose","USA","NY"),
-#     ("Robert","Williams","USA","CA"),
-#     ("Maria","Jones","USA","FL")
-#   ]
-# columns = ["firstname","lastname","country","state"]
-# df = spark.createDataFrame(data = data, schema = columns)
-# # df.show(truncate=False)
-#
-# df.select("firstname",when(df.firstname == 'Michael', 1).otherwise(0)).show(10)
-#
-# dataframe = df.withColumn('new_column', pyspark.F.lit('This is a new column'))
-# dataframe.show(2)
-
+st = time.time()
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 # spark is from the previous example
 
-
 # A text dataset is pointed to by path.
 # The path can be either a single text file or a directory of text files
-path = "./pageviews.txt"
+path = "./Files/pageviews3"
+# df = sparkContext.wholeTextFiles(path)
+
 
 df = spark.read.text(path)
 # df.show(3)
@@ -97,15 +64,14 @@ vals = [('Sirene', 3)]
 
 search_df = spark.createDataFrame(vals, columns)
 
-def findviews(search, df3,search_df):
+
+# Part 1 Calculating total views
+def findtotalviews(search, df3, search_df):
     df4 = df3.filter(df3["page_title"] == search)
-    df4.show(50)
 
     row_list = df4.collect()
     # print(type(row_list[1].__getitem__('views')))
     print(len(row_list))
-
-
 
     # Summing number of views
     view = 0
@@ -120,10 +86,15 @@ def findviews(search, df3,search_df):
 
     newRow = spark.createDataFrame([(search, view)], columns)
     search_df = search_df.union(newRow)
-    search_df.show()
+
     return search_df
 
 
+# This function search pages that exactly matching given word
+def exactmatch(search, df3):
+    df7 = df3.select("page_title", when(df3.page_title == search, 1).otherwise(0))
+    df7.show()
+    # df4 = df3.filter(df3["page_title"] == search)
     # Other Part search algorithms
 
     # df3.select("page_title", when(df3.page_title == 'Barack_Obama', 1).otherwise(0))
@@ -133,15 +104,14 @@ def findviews(search, df3,search_df):
     # dataframe_txt.select(when(dataframe_txt == 'By Lewis Carol')).show(10)
 
 
-# name = 'Siren'
-# findviews(name,df3,search_df)
-# df6 = search_df.filter(search_df["page_title"] == name)
-# df6.show(6)
-# print(len(df6.collect()))
-# if len(df6.collect())>0:
-#     print("found it")
+def partiallymatch(search, df3):
+    search = "%"+search+"%"
+    df8 = df3.select("page_title", df3.page_title.like(search))
+    df8.show()
 
 
+# Part 1 Main Iterative Implementation
+st2 = time.time()
 for x in range(0, len(rowList)):
     name = str(rowList[x].__getitem__('page_title'))
     print(name)
@@ -151,20 +121,30 @@ for x in range(0, len(rowList)):
     if len(df6.collect()) == 0:
         print("Searching views for:")
         print(name, "\n")
-        search_df = findviews(name, df3, search_df)
-    # df6 = search_df.filter(df3["page_title"] == 'Sirene')
-    # print(name)
-    # len(df6)
-    # if name != '':
-    #     print("Searching")
-    #     print(name)
-    #     findviews(name,df3,search_df)
-    # name = ''
+        search_df = findtotalviews(name, df3, search_df)
+
+search_df.show()
+
+et1 = time.time()
+# Part 2 Execution
+print("Exactly Match")
+exactmatch("Sirene", df3)
+et2 = time.time()
+
+# Part 3 Execution
+print("Partially Match")
+partiallymatch("Sjabloon", df3)
+et3 = time.time()
 
 
+total_compile_time = et3 - st
+dataframe_compile_time = st-st2
+part3_compile_time = et3-et2
+part2_compile_time =  et2-et1
+part1_compile_time = et1 - st2
 
-
-
-
-
-
+print('Total Execution time:', total_compile_time, 'seconds')
+print('Reading Dataframe Execution time:', total_compile_time, 'seconds')
+print('Part1 Execution time', part1_compile_time, 'seconds')
+print('Part2 Execution time', part2_compile_time, 'seconds')
+print('Part3 Execution time', part3_compile_time, 'seconds')
