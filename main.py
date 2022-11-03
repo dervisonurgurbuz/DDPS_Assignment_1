b965fa41b6a31f9d7df31d9353b09451aee8b73f
dervisonurgurbuz <onurgurbuz97@gmail.com>
2022-11-03 01:08:44 +0100

    dataset 2

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
 # A text dataset is pointed to by path.
 # The path can be either a single text file or a directory of text files
-path = "./Files/pageviews3"
+path = "./Files/pageviews2"
 # df = sparkContext.wholeTextFiles(path)
 
 
