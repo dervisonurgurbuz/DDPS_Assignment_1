1f3c1b7ac5259b119b0e531f074da772787a41d0
Jonathan S <jonastrijker@gmail.com>
2022-11-04 22:02:32 +0100

    hdfs config test

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-start-dfs.sh
-stop-all.sh
+mkdir /var/scratch/$USER/hadoop_hdfs
+cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/hdfs-site.xml /var/scratch/$USER/hadoop/etc/hadoop/hdfs-site.xml
+
+
+# start-dfs.sh
+# stop-all.sh
