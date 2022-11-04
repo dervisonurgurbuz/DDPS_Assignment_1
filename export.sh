df8f96930de487441addf65ca77b0b9acb4ee35c
Jonathan S <jonastrijker@gmail.com>
2022-11-04 22:50:59 +0100

    mess

--- export.sh
+++ export.sh
@@ -22,5 +22,14 @@ export SPARK_HOME=/var/scratch/$USER/spark;
 export HADOOP_HOME=/var/scratch/$USER/hadoop;
 export PATH=${SPARK_HOME}/bin:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:${PATH};
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip;
+
+export HADOOP_MAPRED_HOME=$HADOOP_HOME 
+export HADOOP_COMMON_HOME=$HADOOP_HOME 
+
+export HADOOP_HDFS_HOME=$HADOOP_HOME 
+export YARN_HOME=$HADOOP_HOME 
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin 
+export HADOOP_INSTALL=$HADOOP_HOME 
 EOF
 source ~/.bashrc
--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir /var/scratch/$USER/hadoop_hdfs
+mkdir /var/scratch/$USER/hadoop_hdfs/namenode
+mkdir /var/scratch/$USER/hadoop_hdfs/datanode
 cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/hdfs-site.xml /var/scratch/$USER/hadoop/etc/hadoop/hdfs-site.xml
 cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/core-site.xml /var/scratch/$USER/hadoop/etc/hadoop/core-site.xml
-
-
-# start-dfs.sh
-# stop-all.sh
+cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/yarn-site.xml /var/scratch/$USER/hadoop/etc/hadoop/yarn-site.xml
+cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/mapred-site.xml /var/scratch/$USER/hadoop/etc/hadoop/mapred-site.xml
