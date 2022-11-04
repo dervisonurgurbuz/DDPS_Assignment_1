30b758884eac5a2dfcd291424378d1bcaa5c083f
Jonathan S <jonastrijker@gmail.com>
2022-11-04 19:59:15 +0100

    hadoop streaming test

---
+++ deploy_hadoop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e # The set command allows you to manage certain flags and characteristics to influence how your bash scripts behave.
+
+if [[ $# -lt 1 ]] ; then
+	echo ""
+	echo "usage: source deploy_hadoop.sh [nodes]"
+	echo "for example: source deploy_hadoop.sh node105,node106,node107"
+	echo ""
+	exit 1
+fi
+
+echo "Deploying spark on ${1}"
+nodes=${1}
+IFS=',' read -ra node_list <<< "$nodes"; unset IFS
+master=${node_list[0]}
+worker=${node_list[@]:1}
+echo "master is "$master
+echo "worker is "$worker
+
+#Comment out these lines if you already downloaded them.
+#wget -O /var/scratch/$USER/spark-3.1.2-bin-hadoop2.7.tgz https://archive.apache.org/dist/spark/spark-3.1.2/spark-3.1.2-bin-hadoop2.7.tgz && \
+#tar -xf /var/scratch/$USER/spark-3.1.2-bin-hadoop2.7.tgz -C /var/scratch/$USER && mv /var/scratch/$USER/spark-3.1.2-bin-hadoop2.7 /var/scratch/$USER/spark
+#wget -O /var/scratch/$USER/openjdk-11.0.2_linux-x64_bin.tar.gz https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz && \
+#tar -zxf /var/scratch/$USER/openjdk-11.0.2_linux-x64_bin.tar.gz -C /var/scratch/$USER
+
+# curl -L -o "/var/scratch/$USER/spark/jars/graphframes-0.8.2-spark3.2-s_2.12.jar" https://repos.spark-packages.org/graphframes/graphframes/0.8.2-spark3.2-s_2.12/graphframes-0.8.2-spark3.2-s_2.12.jar
+# wget -O /var/scratch/$USER/hadoop-2.7.0.tar.gz https://archive.apache.org/dist/hadoop/common/hadoop-2.7.0/hadoop-2.7.0.tar.gz && \
+# tar -zxf /var/scratch/$USER/hadoop-2.7.0.tar.gz -C /var/scratch/$USER && mv /var/scratch/$USER/hadoop-2.7.0 /var/scratch/$USER/hadoop
+
+source export.sh
+
+cd /var/scratch/$USER/hadoop/etc/hadoop # && cp spark-env.sh.template spark-env.sh && cp workers.template workers
+sleep 3
+echo "export JAVA_HOME=/var/scratch/$USER/jdk-11.0.2" >> mapred-env.sh
+echo "export JAVA_HOME=/var/scratch/$USER/jdk-11.0.2" >> hadoop-env.sh
+echo "export JAVA_HOME=/var/scratch/$USER/jdk-11.0.2" >> yarn-env.sh
+
+
+echo "export SPARK_MASTER_HOST=$master" >> spark-env.sh
+echo "$worker" > workers
+
+ssh $master "cd /var/scratch/$USER/spark && ./bin/spark-submit /var/scratch/$USER/DDPS_Assignment_1/pyspark_pagerank.py ${#node_list[@]} soc-Epinions1 10" 
