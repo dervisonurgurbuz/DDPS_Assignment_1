1e307a805d575226d52fccc6ed83bed175b895cb
Jonathan S <jonastrijker@gmail.com>
2022-11-04 20:02:20 +0100

    sbin hadoop

--- export.sh
+++ export.sh
@@ -20,7 +20,7 @@ alias java="$JAVA_HOME/bin/java"
 #SPARK & HADOOP
 export SPARK_HOME=/var/scratch/$USER/spark;
 export HADOOP_HOME=/var/scratch/$USER/hadoop;
-export PATH=${SPARK_HOME}/bin:${HADOOP_HOME}/bin:${PATH};
+export PATH=${SPARK_HOME}/bin:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:${PATH};
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip;
 EOF
 source ~/.bashrc
