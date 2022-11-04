d3a096dab85754b9199899bfeae918419d53e262
Jonathan S <jonastrijker@gmail.com>
2022-11-04 23:29:38 +0100

    sdgefgdfg

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir /var/scratch/$USER/hadoop_hdfs
-mkdir /var/scratch/$USER/hadoop_hdfs/namenode
-mkdir /var/scratch/$USER/hadoop_hdfs/datanode
-mkdir /var/scratch/$USER/hadoop_hdfs/temp
+mkdir -p $HADOOP_HOME/dfs
+mkdir -p $HADOOP_HOME/dfs/name
+mkdir -p $HADOOP_HOME/dfs/name/data
 cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/hdfs-site.xml /var/scratch/$USER/hadoop/etc/hadoop/hdfs-site.xml
 cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/core-site.xml /var/scratch/$USER/hadoop/etc/hadoop/core-site.xml
 cp /var/scratch/$USER/DDPS_Assignment_1/hadoop_test/xml/yarn-site.xml /var/scratch/$USER/hadoop/etc/hadoop/yarn-site.xml
