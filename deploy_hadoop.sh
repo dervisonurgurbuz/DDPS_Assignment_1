b707f90ac834b732d7264a92d7b2cae362b67ca1
Jonathan S <jonastrijker@gmail.com>
2022-11-05 16:50:17 +0100

    asdassadas

--- deploy_hadoop.sh
+++ deploy_hadoop.sh
@@ -44,4 +44,4 @@ cd .. && git clone https://github.com/danielepantaleone/hadoop-pagerank.git && c
 javac -classpath ${HADOOP_CLASSPATH} -d ./ src/it/uniroma1/hadoop/pagerank/PageRank.java src/it/uniroma1/hadoop/pagerank/job1/PageRankJob1Mapper.java src/it/uniroma1/hadoop/pagerank/job1/PageRankJob1Reducer.java src/it/uniroma1/hadoop/pagerank/job2/PageRankJob2Mapper.java src/it/uniroma1/hadoop/pagerank/job2/PageRankJob2Reducer.java src/it/uniroma1/hadoop/pagerank/job3/PageRankJob3Mapper.java 
 jar -cf it/pagerank.jar it/
 cd ..
-hadoop jar hadoop-pagerank/it/pagerank.jar it.uniroma1.hadoop.pagerank.PageRank --input /input/soc-Epinions1.txt --output /output --count {}
+hadoop jar hadoop-pagerank/it/pagerank.jar it.uniroma1.hadoop.pagerank.PageRank --input /input/soc-Epinions1.txt --output /output --count ${1}
