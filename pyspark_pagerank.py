aa04563911c1b164ed6763719bc9fd4c5ccbc87b
Jonathan S <jonastrijker@gmail.com>
2022-11-07 18:21:22 +0100

    metrics

--- pyspark_pagerank.py
+++ pyspark_pagerank.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     from sparkmeasure import StageMetrics
 
     stagemetrics = StageMetrics(spark)
-    stagemetrics.begin()
+    
     # Loads in input file. It should be in format of:
     #     URL         neighbor URL
     #     URL         neighbor URL
@@ -83,20 +83,21 @@ if __name__ == "__main__":
 
     # Calculates and updates URL ranks continuously using PageRank algorithm.
     for iteration in range(int(sys.argv[2])):
-
+        stagemetrics.begin()
         # Calculates URL contributions to the rank of other URLs.
         contribs = links.join(ranks).flatMap(
             lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
 
         # Re-calculates URL ranks based on neighbor contributions.
         ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
+        stagemetrics.end()
+        stagemetrics.print_report()
 
     # Collects all URL ranks and dump them to console.
     for (link, rank) in ranks.collect():
         print("%s has rank: %s." % (link, rank))
         break
-    stagemetrics.end()
-    stagemetrics.print_report()
+
     spark.stop()
     # End time 
     end = time.perf_counter()
