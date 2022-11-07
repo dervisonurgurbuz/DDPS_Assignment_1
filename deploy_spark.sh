f3080270acf02ad9e4720f3b3f9174278ef6b7b5
Jonathan S <jonastrijker@gmail.com>
2022-11-07 02:14:49 +0100

    absolute paths

--- deploy_spark.sh
+++ deploy_spark.sh
@@ -36,6 +36,6 @@ echo "export JAVA_HOME=/var/scratch/$USER/jdk-11.0.2" >> spark-env.sh
 echo "export SPARK_MASTER_HOST=$master" >> spark-env.sh
 echo "$worker" > workers
 
-mv pyspark_pagerank.py ../spark/examples/src/main/pagerank.py
+mv /var/scratch/$USER/DDPS_Assignment_1/pyspark_pagerank.py /var/scratch/$USER/DDPS_Assignment_1/spark/examples/src/main/pagerank.py
 
 ssh $master "cd /var/scratch/$USER/spark && ./bin/spark-submit examples/src/main/python/pagerank.py /var/scratch/$USER/DDPS_Assignment_1/datasets/${2} ${3} ${#node_list[@]}" 
