44679e95ef8b4d61b474358adf9abc2c5c2bc7b2
Jonathan S <jonastrijker@gmail.com>
2022-11-07 04:08:35 +0100

    asdasdasd

--- pyspark_pagerank.py
+++ pyspark_pagerank.py
@@ -62,13 +62,13 @@ if __name__ == "__main__":
     #     URL         neighbor URL
     #     URL         neighbor URL
     #     ...
-    if (sys.argv[1] == "soc-Epinions1.txt") :
-      sep = '\t'
-    elif (sys.argv[1] == "wiki-topcats.txt") :
-      sep = ' '
-    else :
-      sep = ','
-    lines = spark.read.text(sys.argv[1], lineSep=sep).rdd.map(lambda r: r[0])
+    # if (sys.argv[1] == "soc-Epinions1.txt") :
+    #   sep = '\t'
+    # elif (sys.argv[1] == "wiki-topcats.txt") :
+    #   sep = ' '
+    # else :
+    #   sep = ','
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
 
     # Loads all URLs from input file and initialize their neighbors.
     links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cache()
